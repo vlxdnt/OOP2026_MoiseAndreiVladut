0ce38c40001b50347caba18f70a15e68fffd1aaf
vlxdnt <r3ptil4@protonmail.com>
2026-02-20 17:20:10 +0200

    Added safety nets and optimized the sorting

--- LAB1/2.c
+++ LAB1/2.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
 void copy(char* dest, char* src);
-int length(char* sir);
-int compare(char* str1, char* str2);
-void sort(char matrix[][255], int rows);
+int length(const char* str);
+int compare(const char* str1, const char* str2);
+void mergePointers(char* ptrs[], int left, int mid, int right);
+void mergeSortPointers(char* ptrs[], int left, int right);
 
 int main()
 {
     char word[255];
     char words[255][255];
+    char* ptrs[255];
     int nrWords;
     scanf("%d", &nrWords);
-    int tmpCuv = 0;
-    while (scanf("%s", word)) {
-        copy(words[tmpCuv], word);
-        tmpCuv++;
-        if (tmpCuv >= nrWords)
+    int tmpCnt = 0;
+    while (scanf("%254s", word)) { // %254s pt a preveni overflow
+        copy(words[tmpCnt], word);
+        ptrs[tmpCnt] = words[tmpCnt];
+        tmpCnt++;
+        if (tmpCnt >= nrWords)
             break;
     }
-    sort(words, nrWords);
+    mergeSortPointers(ptrs, 0, nrWords - 1);
     printf("---------------------------------\n");
     for (int i = 0; i < nrWords; i++) {
-        printf("%s\n", words[i]);
+        printf("%s\n", ptrs[i]);
     }
     return 0;
 }
@@ -37,15 +40,15 @@ void copy(char* dest, char* src) {
 }
 
 // functie pt lungime
-int length(char* sir) {
+int length(const char* str) {
     int index = 0;
-    while (sir[index] != '\0')
+    while (str[index] != '\0')
         index++;
     return index;
 }
 
 // functie de comparare a doua stringuri 
-int compare(char* str1, char* str2) {
+int compare(const char* str1, const char* str2) {
     int index = 0;
     while (str1[index] != '\0' && str2[index] != '\0') {
         if (str1[index] < str2[index])
@@ -62,23 +65,46 @@ int compare(char* str1, char* str2) {
         return 1;
 }
 
-// sortez si daca au aceeasi lungime le sortez lexicografic
-void sort(char matrix[][255], int rows) {
-    for (int i = 0; i < rows - 1; i++)
-        for (int j = i + 1; j < rows; j++) {
+void mergePointers(char* ptrs[], int left, int mid, int right) {
+    int i = left, j = mid + 1, k = 0;
+    char* tmp[255];
 
-            if (length(matrix[i]) < length(matrix[j])) {
-                char tmp[255];
-                copy(tmp, matrix[i]);       
-                copy(matrix[i], matrix[j]); 
-                copy(matrix[j], tmp);
-            }
-            
-            else if (length(matrix[i]) == length(matrix[j]) && compare(matrix[i], matrix[j]) > 0) {
-                char tmp[255];
-                copy(tmp, matrix[i]);       
-                copy(matrix[i], matrix[j]); 
-                copy(matrix[j], tmp);
-            }
+    while (i <= mid && j <= right) {
+        int leni = length(ptrs[i]);
+        int lenj = length(ptrs[j]);
+
+        if (leni > lenj || (leni == lenj && compare(ptrs[i], ptrs[j]) < 0)) {
+            tmp[k] = ptrs[i];
+            i++;
+        } else {
+            tmp[k] = ptrs[j];
+            j++;
         }
+        k++;
+    }
+
+    while (i <= mid) {
+        tmp[k] = ptrs[i];
+        i++;
+        k++;
+    }
+
+    while (j <= right) {
+        tmp[k] = ptrs[j];
+        j++;
+        k++;
+    }
+
+    for (i = left, k = 0; i <= right; i++, k++) {
+        ptrs[i] = tmp[k];
+    }
+}
+
+void mergeSortPointers(char* ptrs[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSortPointers(ptrs, left, mid);
+        mergeSortPointers(ptrs, mid + 1, right);
+        mergePointers(ptrs, left, mid, right);
+    }
 }
