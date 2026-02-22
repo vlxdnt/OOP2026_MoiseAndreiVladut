af3b3b6481c06c2cf28e4ea42efc568cf9e7a2f4
vlxdnt <r3ptil4@protonmail.com>
2026-02-22 23:19:36 +0200

    Fixed isPrime function

--- LAB1/3.cpp
+++ LAB1/3.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 bool isPrime(int n)
 {
-    for (int tr = 2; tr < n / 2; tr++)
-        if ((n % tr ) == 0)
+    for (int tr = 2; tr < n / ((n == 4) ? 1 : 2); tr++)
+        if ((n % tr) == 0)
             return false;
     return true;
 }
